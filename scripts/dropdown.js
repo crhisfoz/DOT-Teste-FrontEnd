13245338aaef24f59057b11322cb7e3b344c28d8
_crhis_ <crhisnokia@live.com>
2022-12-03 22:25:04 -0300

    style: estilizando dropdown

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,8 +1,8 @@
-function showTest(idText, idButton) {
+/* function showTest() {
     const textValue = document.getElementById(idText)
     const button = document.getElementById("button-show")
     const containerButton = document.getElementById("container-button")
-     const containerInfo =  document.getElementById("container-info")
+    const containerInfo = document.getElementById("container-info")
 
     const imageValue = document.getElementById("button-image")
     console.log(imageValue)
@@ -19,7 +19,75 @@ function showTest(idText, idButton) {
         button.style.display = "block"
         containerButton.style.height = "3rem"
         containerInfo.style.height = "0"
-  
+
     }
 }
 
+ */
+
+function showTest(idBoxText, idButton, IdContainerButton) {
+
+    let boxText;
+
+    switch (idBoxText) {
+        case "container-cards-left": 
+        boxText = document.getElementById(idBoxText)
+        break;
+        
+        case "container-cards-center": 
+        boxText = document.getElementById(idBoxText)
+        break;
+
+            
+        case "container-cards-right": 
+        boxText = document.getElementById(idBoxText)
+        break;
+        default:
+    };
+    
+    let button; 
+    switch (idButton) {
+        case "button-dropdown-left": 
+        button = document.getElementById(idButton); 
+        break;
+        
+        case "button-dropdown-center": 
+        button = document.getElementById(idButton); 
+        break;
+  
+        case "button-dropdown-right": 
+        button = document.getElementById(idButton) ;
+
+        break;
+        default:
+    };
+
+    let containerButton;
+
+    switch (IdContainerButton) {
+        case "dropdown-container-button-left": 
+        containerButton = document.getElementById(IdContainerButton); 
+        break;
+        
+        case "dropdown-container-button-center": 
+        containerButton = document.getElementById(IdContainerButton); 
+        break;
+  
+        case "dropdown-container-button-right": 
+        containerButton = document.getElementById(IdContainerButton) ;
+
+        break;
+        default:
+    };
+
+
+      const iconArrow = ["../assets/icons/icon-arrow-down.svg","../assets/icons/icon-arrow-up.svg"];
+
+    boxText.style.display = "block"
+    button.style.backgroundColor = " #7a2b58"
+    button.firstElementChild.src = iconArrow[1];
+    containerButton.style.backgroundColor =  "#ffcc33"
+    
+  }
+ 
+   
