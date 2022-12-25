34c63fbdecdc59ce8cdb773d0d6d58992cbfb0ae
_crhis_ <crhisnokia@live.com>
2022-12-25 11:16:42 -0300

    refactor: refatorando script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,18 @@
 const buttons = document.querySelectorAll("button")
 
 for (let i = 0; i < buttons.length; i++) {
-    const button = buttons[i];
-    button.addEventListener("click", (e) => {
+
+    buttons[i].addEventListener("click", (e) => {
         let text = document.getElementsByClassName("text-hidden");
-
-        let button = document.getElementById("button")
-
         for (let j = 0; j < text.length; j++) {
             text[j].style.display = "none";
+            buttons[j].parentElement.querySelectorAll("img")[0].src = "../assets/icons/icon-arrow-down.svg"
+
+            console.log(buttons[i].parentNode.querySelectorAll("img")[0].src = "../assets/icons/icon-arrow-down.svg")
+            buttons[j].style.backgroundColor = " #fff"
         }
         text[i].style.display = "block";
+        buttons[i].parentNode.querySelectorAll("img")[0].src = "../assets/icons/icon-arrow-up.svg"
+        buttons[i].style.backgroundColor = " #7a2b58"
     })
-
 }
